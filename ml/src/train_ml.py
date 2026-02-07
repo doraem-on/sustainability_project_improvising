847f45901aa0157b11ef80f871b369d54d4f855b
PARTHDESHMUKH2005 <parthdeshmukh036@gmail.com>
2026-02-07 22:07:03 +0530

    Update train_ml.py

--- ml/src/train_ml.py
+++ ml/src/train_ml.py
@@ -1,34 +1,43 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-import joblib
+from sklearn.metrics import r2_score, mean_absolute_error, accuracy_score
+import xgboost as xgb
+import joblib 
 
-
-df = pd.read_csv("data/processed/final_dataset.csv")
-
-
-
+df  = pd.read_csv('solar_panel_combined_dataset.csv')
+df['Label'] = df['Label (Yes/No)'].map({'Yes':1,'No':0})
+df.drop('panel_id', axis=1, inplace=True)
 for col in df.columns:
     if df[col].dtype == 'object':
-        if df[col].nunique() == 2:  
-            df[col] = df[col].map({'Yes': 1, 'No': 0})
-        else:
-            df = pd.get_dummies(df, columns=[col], drop_first=True)
+        df = pd.get_dummies(df, columns=[col], drop_first=True)
+X_eff = df.drop(['efficiency', 'Label'], axis=1)  
+y_eff = df['efficiency']
+X_train_eff, X_test_eff, y_train_eff, y_test_eff = train_test_split(X_eff, y_eff, test_size=0.2, random_state=42)
+#efficieny one
+model_eff = xgb.XGBRegressor(n_estimators=300, learning_rate=0.1, max_depth=5, random_state=42, objective='reg:squarederror')
+model_eff.fit(X_train_eff, y_train_eff)
+y_pred_eff = model_eff.predict(X_test_eff)
+r2_eff = r2_score(y_test_eff, y_pred_eff)
+mae_eff = mean_absolute_error(y_test_eff, y_pred_eff)
+print(f"Efficiency Model - R²: {r2_eff:.4f} (Target >0.94), MAE: {mae_eff:.4f}")
+#sustainabilty one
+X_site = df.drop(['efficiency', 'Label'], axis=1)  
+y_site = df['Label']
+X_train_site, X_test_site, y_train_site, y_test_site = train_test_split(X_site, y_site, test_size=0.2, random_state=42)
 
+model_site = xgb.XGBClassifier(n_estimators=100, learning_rate=0.1, max_depth=4, random_state=42)
+model_site.fit(X_train_site, y_train_site)
 
-if 'efficiency' not in df.columns:
-    raise ValueError("Target column 'efficiency' not found in dataset")
+y_pred_site = model_site.predict(X_test_site)
+acc_site = accuracy_score(y_test_site, y_pred_site)
+print(f"Suitability Model Accuracy: {acc_site:.4f}")
 
-X = df.drop("efficiency", axis=1)
-y = df["efficiency"]
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
-model = RandomForestRegressor(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
-
-
-joblib.dump(model, "ml/models/efficiency_model.pkl")
-print("Model trained and saved successfully!")
+#save for first
+joblib.dump(model_eff, "efficiency_model.pkl")
+joblib.dump(X_eff.columns.tolist(), "feature_columns.pkl")
+#save for 2nd
+joblib.dump(model_site, "suitability_model.pkl")
+joblib.dump(X_site.columns.tolist(), "site_feature_columns.pkl")
+print("="*100)
+print("all pkl models done")
+print("="*100)
